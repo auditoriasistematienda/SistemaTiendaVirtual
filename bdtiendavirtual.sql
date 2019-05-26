5e208baa3752b050faa5863440cbad44f05f1179
RedJohn1426 <alex98rodriguezromero@gmail.com>
2019-05-26 18:37:39 -0500

    Se modifica base de datos

--- bdtiendavirtual.sql
+++ bdtiendavirtual.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-05-2019 a las 22:19:55
--- Versión del servidor: 10.1.35-MariaDB
--- Versión de PHP: 7.2.9
+-- Tiempo de generación: 27-05-2019 a las 01:36:10
+-- Versión del servidor: 10.1.39-MariaDB
+-- Versión de PHP: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,95 +32,24 @@ CREATE TABLE `clientes` (
   `cli_id` int(11) NOT NULL,
   `cli_dni` varchar(8) NOT NULL,
   `cli_apellidos` varchar(100) NOT NULL,
-  `cli_nombres` varchar(100) NOT NULL
+  `cli_nombres` varchar(100) NOT NULL,
+  `cli_email` varchar(100) NOT NULL,
+  `cli_user` varchar(20) NOT NULL,
+  `cli_password` varchar(30) NOT NULL,
+  `cli_tipo` varchar(20) NOT NULL,
+  `cli_activo` int(11) NOT NULL,
+  `cli_direccion` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `clientes`
 --
 
-INSERT INTO `clientes` (`cli_id`, `cli_dni`, `cli_apellidos`, `cli_nombres`) VALUES
-(1, '45207860', 'MENDOZA QUISPE', 'PERCY JULIO'),
-(2, '45076456', 'CAMA CORREA', 'KELVIN'),
-(3, '25416070', 'CEPEDA BURGOS', 'REBECA LEONOR'),
-(4, '45076845', 'RAMIREZ SALDAÑA', 'MONICA');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `detalleventas`
---
-
-CREATE TABLE `detalleventas` (
-  `dv_idventa` int(11) NOT NULL,
-  `dv_idproducto` int(11) NOT NULL,
-  `dv_cantidad` int(11) NOT NULL,
-  `dv_total` decimal(6,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `detalleventas`
---
-
-INSERT INTO `detalleventas` (`dv_idventa`, `dv_idproducto`, `dv_cantidad`, `dv_total`) VALUES
-(4, 3, 2, '11.00'),
-(8, 2, 3, '18.00'),
-(8, 4, 2, '7.00');
-
---
--- Disparadores `detalleventas`
---
-DELIMITER $$
-CREATE TRIGGER `tr_updStockVenta` AFTER INSERT ON `detalleventas` FOR EACH ROW BEGIN
-	UPDATE productos SET prod_stock = prod_stock -
-NEW.dv_cantidad
-	WHERE productos.prod_id = NEW.dV_idproducto;
-END
-$$
-DELIMITER ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `productos`
---
-
-CREATE TABLE `productos` (
-  `prod_id` int(11) NOT NULL,
-  `prod_nombre` varchar(100) NOT NULL,
-  `prod_precio` decimal(6,2) NOT NULL,
-  `prod_stock` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `productos`
---
-
-INSERT INTO `productos` (`prod_id`, `prod_nombre`, `prod_precio`, `prod_stock`) VALUES
-(2, 'Atún Florida Filete', '6.00', 47),
-(3, 'Atún Fanny Trozos', '5.50', 78),
-(4, 'Arroz Costeño 1kg', '3.50', 73);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ventas`
---
-
-CREATE TABLE `ventas` (
-  `ven_id` int(11) NOT NULL,
-  `ven_idcliente` int(11) NOT NULL,
-  `ven_fecha` date NOT NULL,
-  `ven_total` decimal(6,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `ventas`
---
-
-INSERT INTO `ventas` (`ven_id`, `ven_idcliente`, `ven_fecha`, `ven_total`) VALUES
-(4, 2, '2019-05-26', '11.00'),
-(8, 1, '2019-05-26', '25.00');
+INSERT INTO `clientes` (`cli_id`, `cli_dni`, `cli_apellidos`, `cli_nombres`, `cli_email`, `cli_user`, `cli_password`, `cli_tipo`, `cli_activo`, `cli_direccion`) VALUES
+(1, '45207860', 'MENDOZA QUISPE', 'PERCY JULIO', '', '', '', '', 0, ''),
+(2, '45076456', 'CAMA CORREA', 'KELVIN', '', '', '', '', 0, ''),
+(3, '25416070', 'CEPEDA BURGOS', 'REBECA LEONOR', '', '', '', '', 0, ''),
+(4, '45076845', 'RAMIREZ SALDAÑA', 'MONICA', '', '', '', '', 0, '');
 
 --
 -- Índices para tablas volcadas
@@ -133,26 +62,6 @@ ALTER TABLE `clientes`
   ADD PRIMARY KEY (`cli_id`);
 
 --
--- Indices de la tabla `detalleventas`
---
-ALTER TABLE `detalleventas`
-  ADD KEY `dv_idventa` (`dv_idventa`),
-  ADD KEY `dv_idproducto` (`dv_idproducto`);
-
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`prod_id`);
-
---
--- Indices de la tabla `ventas`
---
-ALTER TABLE `ventas`
-  ADD PRIMARY KEY (`ven_id`),
-  ADD KEY `ven_idcliente` (`ven_idcliente`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -161,35 +70,6 @@ ALTER TABLE `ventas`
 --
 ALTER TABLE `clientes`
   MODIFY `cli_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `productos`
---
-ALTER TABLE `productos`
-  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `ventas`
---
-ALTER TABLE `ventas`
-  MODIFY `ven_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `detalleventas`
---
-ALTER TABLE `detalleventas`
-  ADD CONSTRAINT `detalleventas_ibfk_1` FOREIGN KEY (`dv_idproducto`) REFERENCES `productos` (`prod_id`),
-  ADD CONSTRAINT `detalleventas_ibfk_2` FOREIGN KEY (`dv_idventa`) REFERENCES `ventas` (`ven_id`);
-
---
--- Filtros para la tabla `ventas`
---
-ALTER TABLE `ventas`
-  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`ven_idcliente`) REFERENCES `clientes` (`cli_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
