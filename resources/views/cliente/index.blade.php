a957074f62b82de91281f7fe4b26a6b55542ae86
JOGL3 <Jhosep.lexar@gmail.com>
2019-05-27 17:31:09 -0500

    Cliente modifica correctamente

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -4,6 +4,7 @@
     <div class="row">
         <div class="col-xl-6">
             <a href="{{url('cliente/create')}}" class="btn btn-primary">Registrar Cliente</a>
+            
         </div>
         <div class="col-xl-6">
             @if (session('status'))
@@ -34,8 +35,8 @@
                         <td>{{$cli->cli_apellidos}}</td>
                         <td>{{$cli->cli_nombres}}</td>
                         <td>
-                          <a href="" class="btn btn-sm btn-warning"> <i class="fa fa-edit"></i> </a>
-                          <a href="" class="btn btn-sm btn-danger"> <i class="fa fa-trash"></i> </a>
+                        <a href="{{url('cliente/'.$cli->cli_id.'/edit')}}" class="btn btn-sm btn-warning">Editar</i> </a> 
+                          <a href="{{url('cliente')}}" class="btn btn-sm btn-danger"> <i class="fa fa-trash"></i> </a>
                         </td>
                     </tr>
                     @endforeach
