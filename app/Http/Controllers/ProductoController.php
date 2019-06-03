e5d2e970ce282dedc0eda0efab2515662a5a5457
jorgejlrr <jorge14.jlrr@gmail.com>
2019-06-03 17:19:39 -0500

    Se agrega validaciones

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -50,10 +50,10 @@ class ProductoController extends Controller
     public function store(Request $request)
     {
       $this->validate($request,[
-        'prod_nombre' => 'required|max:50',
-        'prod_slug' => 'required|max:50' ,
-        'prod_descripcion' => 'required|max:50' ,
-        'prod_extract' => 'required|max:50' ,
+        'prod_nombre' => 'required|min:3|max:50|alpha',
+        'prod_slug' => 'required|min:3|max:50|alpha' ,
+        'prod_descripcion' => 'required|min:3|max:50|alpha' ,
+        'prod_extract' => 'required|min:3|max:50|alpha' ,
         'prod_precio' => 'required|numeric',
         'prod_stock' => 'required|numeric'
       ]);
