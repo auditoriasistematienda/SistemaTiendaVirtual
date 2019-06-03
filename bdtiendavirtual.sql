ba9c3cb577faeb605b7e842ed4f525eda0274d71
jorgejlrr <jorge14.jlrr@gmail.com>
2019-06-03 18:33:45 -0500

    new BD

--- bdtiendavirtual.sql
+++ bdtiendavirtual.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-06-2019 a las 01:30:04
+-- Tiempo de generación: 04-06-2019 a las 01:32:48
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.3
 
